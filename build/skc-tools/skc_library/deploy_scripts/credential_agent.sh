9073e9e1efc8e933c4949e9af968ab450bc9becb
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-03-17 05:02:51 -0700

    ISECL-14841: addded credential_agent in containers

--- build/skc-tools/skc_library/deploy_scripts/credential_agent.sh
+++ build/skc-tools/skc_library/deploy_scripts/credential_agent.sh
@@ -14,16 +14,15 @@ SKCLIB_INST_PATH=/opt/skc
 KMS_NPM_PATH=$SKCLIB_INST_PATH/etc/kms_npm.ini
 CREDENTIAL_PATH=$SKCLIB_INST_PATH/etc/credential_agent.ini
 CURL_OPTS="-s -k"
-SGX_DEFAULT_PATH=/etc/sgx_default_qcnl.conf
 
 update_credential_ini()
 {
-	sed -i "s|server=.*|server=https:\/\/$KBS_HOSTNAME:9443|g" $KMS_NPM_PATH
+	sed -i "s|server=.*|server=https:\/\/$KBS_HOSTNAME:${KBS_PORT:-9443}|g" $KMS_NPM_PATH
 	sed -i "s|request_params=.*|request_params=\"\/CN=$SKC_USER\"|g" $CREDENTIAL_PATH
 	sed -i "s|server=.*|server=$CMS_IP|g" $CREDENTIAL_PATH
-	sed -i "s|port=.*|port=8445|g" $CREDENTIAL_PATH
+	sed -i "s|port=.*|port=${CMS_PORT:-8445}|g" $CREDENTIAL_PATH
 	sed -i "s|^token=.*|token=\"$SKC_TOKEN\"|g" $CREDENTIAL_PATH
-	curl $CURL_OPTS -H 'Accept:application/x-pem-file' https://$CMS_IP:8445/cms/v1/ca-certificates > $SKCLIB_INST_PATH/store/cms-ca.cert || exit 1
+	curl $CURL_OPTS -H 'Accept:application/x-pem-file' https://$CMS_IP:${CMS_PORT:-8445}/cms/v1/ca-certificates > $SKCLIB_INST_PATH/store/cms-ca.cert || exit 1
 	if [ $? -ne 0 ]; then
 		echo "${red} could not get Certificate Management Service Root CA Certificate ${reset}"
 		exit 1
@@ -42,7 +41,7 @@ run_credential_agent()
 
 update_dcap_qcnl_conf()
 {
-	sed -i "s|PCCS_URL=.*|PCCS_URL=https:\/\/$CSP_SCS_IP:9000/scs/sgx/certification/v1/|g" $SGX_DEFAULT_PATH
+	sed -i "s|PCCS_URL=.*|PCCS_URL=https:\/\/$CSP_SCS_IP:${SCS_PORT:-9000}/scs/sgx/certification/v1/|g" ${SGX_DEFAULT_PATH:-/etc/sgx_default_qcnl.conf}
 }
 
 update_kbshostname_in_conf_file()
