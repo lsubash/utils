af74ea51cbeba5890e09a9c62548916f64a97b3d
Vartika Dwivedi <vartika.dwivedi@intel.com>
2021-04-19 22:07:29 -0700

    Added description in the skc_library untar file

--- build/skc-tools/skc_library/skclib_untar.sh
+++ build/skc-tools/skc_library/skclib_untar.sh
@@ -9,7 +9,8 @@ verify_checksum()
 	fi
 	tar -xf skc_library.tar
 	echo "skc_library untar completed."
-	echo "Please update the skc_library.conf and then run ./deploy_skc_library.sh"
+	echo "Please update create_roles.conf and then run ./skc_library_create_roles.sh. Copy the generated token printed on the console."
+	echo "Please update skc_library.conf with the token obtained from previous step and other details and then run ./skc_library_create_roles.sh"
 }
 
 verify_checksum
