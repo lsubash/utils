9b4c6a5b5ba51454f66b324c13e9fda290655005
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-04-26 08:15:27 -0700

    5.11 kernal changes for container

--- build/skc-tools/sgx_agent/deploy_scripts/agent_container_prereq.sh
+++ build/skc-tools/sgx_agent/deploy_scripts/agent_container_prereq.sh
@@ -9,7 +9,7 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
-cat $KDIR/.config | grep "CONFIG_INTEL_SGX=y" > /dev/null
+cat $KDIR/.config | grep 'CONFIG_INTEL_SGX=y\|CONFIG_X86_SGX=y' > /dev/null
 INKERNEL_SGX=$?
 
 # Check OS and VERSION
@@ -21,16 +21,33 @@ VER=$(cat /etc/os-release | grep ^VERSION_ID | tr -d 'VERSION_ID="')
 
 uninstall_sgx_agent_dependencies()
 {
-        echo "uninstalling sgx psw/qgl and multi-package agent rpm"
-        if [ "$OS" == "rhel" ]; then
-                rpm -qa | grep 'sgx' | xargs rpm -e
-                rm -rf /etc/yum.repos.d/*sgx_rpm_local_repo.repo
-        elif [ "$OS" == "ubuntu" ]; then
-                apt remove -y libsgx-dcap-ql 
+	echo "uninstalling sgx psw/qgl and multi-package agent rpm"
+	if [ "$OS" == "rhel" ]; then
+		dnf remove -y libsgx-dcap-ql libsgx-ra-uefi
+	elif [ "$OS" == "ubuntu" ]; then
+		apt remove -y libsgx-dcap-ql libsgx-ra-uefi
+	fi
+	
+	if [[ "$INKERNEL_SGX" -eq 1 ]]; then
+		DRIVER_VERSION=`modinfo intel_sgx | grep -w 'version:' | awk '{print $2}'`
+		if [ "$DRIVER_VERSION" == "" ]; then
+			echo "SGX DCAP driver not installed"
+			return
+		fi
+		if [ "$DRIVER_VERSION" != "$SGX_DRIVER_VERSION" ]; then
+			echo "uninstalling sgx dcap driver"
+			systemctl stop aesmd
+			sh $SGX_INSTALL_DIR/sgxdriver/uninstall.sh
+			if [[ $? -ne 0 ]]; then
+				echo "${red} sgx dcap driver uninstallation failed. exiting ${reset}"
+				exit 1
+			fi
+			systemctl start aesmd
+		fi
+	else
+                echo "found inbuilt sgx driver, skipping dcap driver uninstallation"
         fi
 
-        echo "uninstalling sgx dcap driver"
-        sh $SGX_INSTALL_DIR/sgxdriver/uninstall.sh
 }
 
 install_prerequisites()
@@ -45,11 +62,16 @@ install_prerequisites()
 
 install_dcap_driver()
 {
-        chmod u+x $SGX_AGENT_BIN/sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin
-        if [[ "$INKERNEL_SGX" -eq 1 ]]; then
-                echo "Installing sgx dcap driver...."
-                ./$SGX_AGENT_BIN/sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin -prefix=$SGX_INSTALL_DIR || exit 1
-                echo "sgx dcap driver installed successfully"
+	chmod u+x $SGX_AGENT_BIN/sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin
+	if [[ "$INKERNEL_SGX" -eq 1 ]]; then
+                DRIVER_VERSION=`modinfo intel_sgx | grep -w 'version:' | awk '{print $2}'`
+                if [ "$DRIVER_VERSION" == "" ] || [ "$DRIVER_VERSION" != "$SGX_DRIVER_VERSION" ]; then
+                        echo "Installing sgx dcap driver...."
+                        ./$SGX_AGENT_BIN/sgx_linux_x64_driver_${SGX_DRIVER_VERSION}.bin -prefix=$SGX_INSTALL_DIR || exit 1
+                        echo "sgx dcap driver installed successfully"
+                else
+                        echo "sgx dcap driver with same version $DRIVER_VERSION already installed, skipping installation..."
+                fi
         else
                 echo "found inbuilt sgx driver, skipping dcap driver installation"
         fi
@@ -64,12 +86,13 @@ install_psw_qgl()
 		rm -rf sgx_rpm_local_repo /etc/yum.repos.d/*sgx_rpm_local_repo.repo
 	elif [ "$OS" == "ubuntu" ]; then
 		echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu/ bionic main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
-		wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add - || exit 1
+		wget -qO - https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
 		apt update -y || exit 1
 		apt install -y libsgx-dcap-ql || exit 1
 	fi
 	echo "${green} sgx psw and qgl installed ${reset}"
 }
+	
 
 uninstall_sgx_agent_dependencies
 install_prerequisites
