b69c6621b09bd1bb82ab5ad1a70c295f3feec462
Raja, Aasikx <aasikx.raja@intel.com>
2021-04-28 07:00:57 -0700

    ISECL-15798: updated enterprise script to install kmip server

--- build/skc-tools/skc_scripts/install_enterprise_skc.sh
+++ build/skc-tools/skc_scripts/install_enterprise_skc.sh
@@ -235,20 +235,31 @@ sed -i "s@^\(KEY_MANAGER\s*=\s*\).*\$@\1$KEY_MANAGER@" ~/kbs.env
 
 if [ $KEY_MANAGER == "KMIP" ]; then
   echo "Updating KMIP Server conf...."
-  sed -i "s@^\(KMIP_SERVER_IP\s*=\s*\).*\$@\1$KMIP_SERVER_IP@" ~/kbs.env
+  sed -i "s@^\(KMIP_SERVER_IP\s*=\s*\).*\$@\1$SYSTEM_IP@" ~/kbs.env
   sed -i "s@^\(KMIP_SERVER_PORT\s*=\s*\).*\$@\1$KMIP_SERVER_PORT@" ~/kbs.env
   sed -i "s@^\(KMIP_CLIENT_CERT_PATH\s*=\s*\).*\$@\1$KMIP_CLIENT_CERT_PATH@" ~/kbs.env
   sed -i "s@^\(KMIP_CLIENT_KEY_PATH\s*=\s*\).*\$@\1$KMIP_CLIENT_KEY_PATH@" ~/kbs.env
   sed -i "s@^\(KMIP_ROOT_CERT_PATH\s*=\s*\).*\$@\1$KMIP_ROOT_CERT_PATH@" ~/kbs.env
 
-  sed -i "s@^\(hostname\s*=\s*\).*\$@\1$KMIP_SERVER_IP@" kbs_script/server.conf
+  sed -i "s@^\(hostname\s*=\s*\).*\$@\1$SYSTEM_IP@" kbs_script/server.conf
   sed -i "s@^\(port\s*=\s*\).*\$@\1$KMIP_SERVER_PORT@" kbs_script/server.conf
 
-  sed -i "s@^\(HOSTNAME_IP\s*=\s*\).*\$@\1'$KMIP_SERVER_IP'@" kbs_script/rsa_create.py
+  sed -i "s@^\(HOSTNAME_IP\s*=\s*\).*\$@\1'$SYSTEM_IP'@" kbs_script/rsa_create.py
   sed -i "s@^\(SERVER_PORT\s*=\s*\).*\$@\1'$KMIP_SERVER_PORT'@" kbs_script/rsa_create.py
   sed -i "s@^\(CERT_PATH\s*=\s*\).*\$@\1'$KMIP_CLIENT_CERT_PATH'@" kbs_script/rsa_create.py
   sed -i "s@^\(KEY_PATH\s*=\s*\).*\$@\1'$KMIP_CLIENT_KEY_PATH'@" kbs_script/rsa_create.py
   sed -i "s@^\(CA_PATH\s*=\s*\).*\$@\1'$KMIP_ROOT_CERT_PATH'@" kbs_script/rsa_create.py
+
+  echo "Installing KMIP Server....."
+  pushd $PWD
+  cd kbs_script/
+  bash install_pykmip.sh
+  if [ $? -ne 0 ]; then
+        echo "${red} KMIP Server installation failed ${reset}"
+        exit 1
+  fi
+  popd
+  echo "KMIP Server installated successfully"
 fi
 
 echo "Installing Key Broker Service...."
