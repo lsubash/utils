12f8821101a7df942ec1f7a02775ef1c617d2589
Johnny Jacob <johnny.jacob@intel.com>
2021-05-26 01:55:09 -0700

    ISECL-14423 Use a challenge between Attesting App and Attested App to ensure the freshness of Quote.

--- tools/sample-sgx-attestation/common/common.go
+++ tools/sample-sgx-attestation/common/common.go
@@ -5,9 +5,11 @@
 package common
 
 const (
-	DummyBearerToken       = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkR1bW15QmVhcmVyVG9rZW4iLCJpYXQiOjE1MTYyMzkwMjJ9.Czed89Gn-nhHGAH2DzVqo453k04eF0PvBuZvvOJeE1Q"
+	AppUsername            = "foobar"
+	AppPassword            = "21345h8askjdf"
 	DefaultAttestedAppHost = "127.0.0.1"
 	DefaultAttestedAppPort = 9999
+	ProtocolTcp            = "tcp4"
 )
 
 const (
@@ -18,15 +20,10 @@ const (
 	SgxQuotePolicyPath    = ConfigDir + "sgx-quote-policy.txt"
 	EndLine               = "\n"
 	VerifyQuote           = "/sgx_qv_verify_quote"
-	GetIdentity           = "/id"
-	PostWrappedSWK        = "/wrapped_swk"
-	PostWrappedMessage    = "/wrapped_message"
-	// Enable client to verify the right cert using SNI.
-	SelfSignedCertSNI = "attested.app"
 	// SWK Size in bytes
 	SWKSize = 32
 	// NonceSize in bits
-	NonceSize = 256
+	NonceSize = 128
 )
 
 // SGX Policy
@@ -38,23 +35,38 @@ const (
 )
 
 // Message definitions
-type UserData struct {
-	Publickey string `json:"public-key"`
+
+const (
+	MsgTypeConnect          uint8 = 1
+	MsgTypePubkeyQuote      uint8 = 2
+	MsgTypePubkeyWrappedSWK uint8 = 3
+	MsgTypeSWKWrappedSecret uint8 = 4
+)
+
+type PayloadConnect struct {
+	Username string
+	Password string
+	Nonce    string
 }
 
-type IdentityResponse struct {
-	Quote    string   `json:"quote"`
-	Userdata UserData `json:"user-data"`
+type PayloadPubkeyQuote struct {
+	Pubkey []byte
+	Quote  []byte
 }
 
-type IdentityRequest struct {
-	Nonce string `json:"nonce"`
+type PayloadPubkeyWrappedSWK struct {
+	WrappedSWK []byte
 }
 
-type WrappedSWKRequest struct {
-	SWK string `json:swk`
+type PayloadSWKWrappedSecret struct {
+	WrappedSecret []byte
 }
 
-type WrappedMessage struct {
-	Message string `json:message`
+type Message struct {
+	Type             uint8
+	ResponseCode     int
+	ConnectRequest   PayloadConnect
+	PubkeyQuote      PayloadPubkeyQuote
+	PubkeyWrappedSWK PayloadPubkeyWrappedSWK
+	SWKWrappedSecret PayloadSWKWrappedSecret
 }
