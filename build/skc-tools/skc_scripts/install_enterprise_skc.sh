dbe13250fa0d962805f6c31ca5db36e025a89025
Raviraj Bhat <raviraj.bhat@intel.com>
2021-06-14 19:14:04 -0700

    ISECL-16564: CSP/ENT service deployment is failing for RHEL Subscription manager

--- build/skc-tools/skc_scripts/install_enterprise_skc.sh
+++ build/skc-tools/skc_scripts/install_enterprise_skc.sh
@@ -14,6 +14,7 @@ OS="$temp"
 VER=$(cat /etc/os-release | grep ^VERSION_ID | tr -d 'VERSION_ID="')
 
 if [[ "$OS" == "rhel" && "$VER" == "8.1" || "$VER" == "8.2" ]]; then
+	dnf install -qy https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/Packages/e/epel-release-8-10.el8.noarch.rpm || exit 1
 	dnf install -qy curl || exit 1
 elif [[ "$OS" == "ubuntu" && "$VER" == "18.04" ]]; then
 	apt install -y curl || exit 1
