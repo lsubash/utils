33db646048715e71ea31fb6356597bbc7de439a4
Rajashekara, Rashmi <rashmi.rajashekara@intel.com>
2021-09-07 22:27:07 -0700

    ISECL-17585: Separate Nginx installation from SKC Library code

--- build/skc-tools/skc_library/deploy_scripts/deployment_prerequisites.sh
+++ build/skc-tools/skc_library/deploy_scripts/deployment_prerequisites.sh
@@ -14,17 +14,13 @@ install_pre_requisites()
 		$PKGMGR install -qy https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Everything/x86_64/os/Packages/l/libgda-5.2.9-6.fc33.x86_64.rpm || exit 1
 		$PKGMGR install -qy https://dl.fedoraproject.org/pub/fedora/linux/releases/33/Everything/x86_64/os/Packages/l/libgda-sqlite-5.2.9-6.fc33.x86_64.rpm || exit 1
 		echo "installing devel packages"
-		$PKGMGR install -qy yum-utils tar wget gcc-c++ kernel-devel kernel-headers dkms make jq protobuf jsoncpp jsoncpp-devel nginx || exit 1
-		groupadd intel
-		usermod -G intel nginx
+		$PKGMGR install -qy yum-utils tar wget gcc-c++ kernel-devel kernel-headers dkms make jq protobuf jsoncpp jsoncpp-devel || exit 1
 		\cp -rpf bin/pkcs11.so $LIB_DIR/engines-1.1/
 		ln -sf $LIB_DIR/engines-1.1/pkcs11.so $LIB_DIR/engines-1.1/libpkcs11.so
 	elif [[ "$OS" == "ubuntu" && "$VER" == "18.04" || "$VER" == "20.04" ]]; then
 		$PKGMGR install -y build-essential ocaml automake autoconf libtool tar wget python libssl-dev || exit 1
 		$PKGMGR-get install -y libcurl4-openssl-dev libprotobuf-dev curl || exit 1
-		$PKGMGR install -y dkms make jq libjsoncpp1 libjsoncpp-dev softhsm libgda-5.0-4 nginx || exit 1
-		groupadd intel
-		usermod -G intel www-data
+		$PKGMGR install -y dkms make jq libjsoncpp1 libjsoncpp-dev softhsm libgda-5.0-4 || exit 1
 		\cp -rpf bin/pkcs11.so /usr/lib/x86_64-linux-gnu/engines-1.1/
 		ln -sf $LIB_DIR/x86_64-linux-gnu/engines-1.1/pkcs11.so $LIB_DIR/x86_64-linux-gnu/engines-1.1/libpkcs11.so
 	else
