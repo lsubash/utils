1cc2b912186eca69b88febaa60e4e1dd8222db99
Rashmi Rajashekar <rashmi.rajashekara@intel.com>
2021-09-21 00:14:36 -0700

    added double quotes

--- build/skc-tools/skc_k8s/skc-bootstrap.sh
+++ build/skc-tools/skc_k8s/skc-bootstrap.sh
@@ -343,8 +343,8 @@ deploy_sqvs() {
   sed -i "s/SQVS_INCLUDE_TOKEN:.*/SQVS_INCLUDE_TOKEN: \"${SQVS_INCLUDE_TOKEN}\"/g" configMap.yml
   sed -i "s#SGX_TRUSTED_ROOT_CA_PATH:.*#SGX_TRUSTED_ROOT_CA_PATH: /tmp/${SGX_TRUSTED_ROOT_CA_FILE}#g" configMap.yml
   sed -i "s/BEARER_TOKEN=.*/BEARER_TOKEN=${BEARER_TOKEN}/g" secrets.txt
-  sed -i "s/SIGN_QUOTE_RESPONSE:.*/SIGN_QUOTE_RESPONSE: ${SIGN_QUOTE_RESPONSE}/g" configMap.yml
-  sed -i "s/RESPONSE_SIGNING_KEY_LENGTH:.*/RESPONSE_SIGNING_KEY_LENGTH: ${RESPONSE_SIGNING_KEY_LENGTH}/g" configMap.yml
+  sed -i "s/SIGN_QUOTE_RESPONSE:.*/SIGN_QUOTE_RESPONSE: \"${SIGN_QUOTE_RESPONSE}\"/g" configMap.yml
+  sed -i "s/RESPONSE_SIGNING_KEY_LENGTH:.*/RESPONSE_SIGNING_KEY_LENGTH: \"${RESPONSE_SIGNING_KEY_LENGTH}\"/g" configMap.yml  
 
   $KUBECTL create secret generic sqvs-secret --from-file=secrets.txt --namespace=isecl
   $KUBECTL create secret generic sqvs-trusted-rootca --from-file=trusted_rootca_files/$SGX_TRUSTED_ROOT_CA_FILE --namespace=isecl
