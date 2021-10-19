8a782005efdeed81366303914080921b117c629c
Harminder Singh <harminderx.singh@intel.com>
2021-10-19 04:11:43 -0700

    Moved to SGX PSW 2.15 and DCAP 1.12

--- build/skc-tools/sgx_agent/build_scripts/download_mpa_uefi_rpm.sh
+++ build/skc-tools/sgx_agent/build_scripts/download_mpa_uefi_rpm.sh
@@ -11,7 +11,11 @@ fetch_mpa_uefi_rpm() {
 		\cp sgx_rpm_local_repo/libsgx-ra-uefi-$MP_RPM_VER.el8.x86_64.rpm $SGX_AGENT_BIN_DIR
 		rm -rf sgx_rpm_local_repo sgx_rpm_local_repo.tgz
 	elif [ "$OS" == "ubuntu" ]; then
-		wget -q https://download.01.org/intel-sgx/sgx-dcap/$DCAP_VERSION/linux/tools/SGXMultiPackageAgent/$OS_FLAVOUR-server/debian_pkgs/libs/libsgx-ra-uefi/libsgx-ra-uefi_1.11.100.2-bionic1_amd64.deb -P $SGX_AGENT_BIN_DIR || exit 1
+		if [ "$VER" == "20.04" ]; then
+			wget -q https://download.01.org/intel-sgx/sgx-dcap/$DCAP_VERSION/linux/tools/SGXMultiPackageAgent/$OS_FLAVOUR-server/debian_pkgs/libs/libsgx-ra-uefi/libsgx-ra-uefi_1.12.100.3-focal1_amd64.deb -P $SGX_AGENT_BIN_DIR || exit 1
+		elif [ "$VER" == "18.04" ]; then
+			wget -q https://download.01.org/intel-sgx/sgx-dcap/$DCAP_VERSION/linux/tools/SGXMultiPackageAgent/$OS_FLAVOUR-server/debian_pkgs/libs/libsgx-ra-uefi/libsgx-ra-uefi_1.12.100.3-bionic1_amd64.deb -P $SGX_AGENT_BIN_DIR || exit 1
+		fi
 	fi
 }
 
